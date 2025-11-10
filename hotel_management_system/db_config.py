06a69d9bdcde03ce3e8cb8c1541d344371d55673
CrocoJRCK <166971724+CrocoJenrick@users.noreply.github.com>
2025-11-10 22:17:44 +0800

    Delete hotel_management_system directory

--- hotel_management_system/db_config.py
+++
@@ -1,8 +0,0 @@
-import MySQLdb
-def get_db_connection():
-    return MySQLdb.connect (
-        host = "localhost",
-        user = "root",
-        passwd = "12345678",
-        database = "hotel_db",
-    )
